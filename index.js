12857ba5620ee0aab4d0016a7bc1000e9be36159
Masud <wubetumasud@gmail.com>
2025-12-15 13:25:18 +0300

    Extracting config logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('config');
+
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -7,15 +7,13 @@ const app = express();
 require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
 
 
 const p = Promise.reject(new Error('something failed miserably'));
 p.then(console.log('done'));
 
-if(!config.get('jwtPrivateKey')) {
-    console.error('FATAL ERROR: jwtPrivateKey is not defined ');
-    process.exit(1);
-}
+
 
 
 
